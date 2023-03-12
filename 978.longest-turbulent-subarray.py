99bce40ab47cb57b611ca6a9657211938a4f78df
t41ty0 <t41ty0@gmail.com>
2023-03-12 22:47:47 +0900

    un done

--- 978.longest-turbulent-subarray.py
+++ 978.longest-turbulent-subarray.py
@@ -7,13 +7,33 @@
 # @lc code=start
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        counter = []
+
+        inquality = []
+
         for i in range(len(arr)-1):
             if arr[i] > arr[i+1]:
-                flag = True
+                inquality.append(1)
             elif arr[i] < arr[i+1]:
-                flag = False
-            elif arr[i] > arr[i+1] and flag == True:
+                inquality.append(0)
+            elif arr[i] == arr[i+1]:
+                inquality.append(2)
 
-        
+        print(inquality)
+        count = 0
+        result = []
+
+        for i in range(len(inquality)-1):
+            print(i, count)
+            if inquality[i] != inquality[i+1] and inquality[i+1] is not 2:
+                count += 1
+            elif inquality[i] == inquality[i+1]:
+                result.append(count)
+                count = 0
+            else:
+                print('what!?')
+        result.append(count)
+        print(result)
+        return max(result)
+
+
 # @lc code=end
